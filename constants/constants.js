029642e13988823046011fcb6543ed4205bbff3d
Hcdev3 <test@handycats.com>
2016-09-30 19:42:39 +0530

    NBN

--- constants/constants.js
+++ constants/constants.js
@@ -9,5 +9,8 @@ module.exports = Object.freeze({
     FACEBOOKLOGIN : 'facebook',
     RECORDS_PER_PAGE: 10,
     ORDERNUMBERLEN:9,
-    EMAILTEMPLATETYPE: [{value:'change_password',lable:'Change Password'},{value:'forget_password',lable:'Forget Password'},{value:'new_order',lable:'New Order'},{value:'new_shipment',lable:'New Shipment'},{value:'registration',lable:'Registration'},{value:'new_account_confirmation_key',lable:'New Account Confirmation Key'}]
+    EMAILTEMPLATETYPE: [{value:'change_password',lable:'Change Password'},{value:'forget_password',lable:'Forget Password'},{value:'new_order',lable:'New Order'},{value:'new_shipment',lable:'New Shipment'},{value:'registration',lable:'Registration'},{value:'new_account_confirmation_key',lable:'New Account Confirmation Key'}],
+    ORDERSTATUSPENDING:'Unfullfilled',
+    ORDERSTATUSPAID:'Paid',
+    ORDERSTATUSREFUND:'Refund'
 });
