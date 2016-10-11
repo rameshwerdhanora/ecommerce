fd46ed208b958f88d96b5dd99f924949b66b5a03
cisnode <prasham.j@cisinlabs.com>
2016-10-11 13:25:09 +0530

    remove duplicate entries of field

--- models/orderDetails.js
+++ models/orderDetails.js
@@ -2,8 +2,6 @@ const mongoose = require('mongoose');
 
 const orderDetailsSchema = new mongoose.Schema({
 	order_id : {type:String,default:''},
-        shop_id : {type:String,default:''},
-        user_id : {type:String,default:''},
 	index : {type:String,default:''},
 	brand_id:{type:String,default:''},
 	user_id:{type:String,default:''},
